47e4426c968fde86b510d8dca593143544e0298a
klanjabrik <meirza.arson@sebangsa.com>
2020-02-24 16:30:19 +0700

    Python 3 compatible

--- backup.py
+++ backup.py
@@ -23,7 +23,7 @@ import helper
 
 try:
   if __name__ != '__main__':
-    print "not allow"
+    print("not allow")
     sys.exit()
 
 
@@ -32,30 +32,30 @@ try:
   
   os.chdir(current_path)
 
-  print sys.argv
+  print(sys.argv)
   if len(sys.argv) != 3:
-    print "wrong argv python backup.py config_file_path upload_file_path"
+    print("wrong argv python backup.py config_file_path upload_file_path")
     sys.exit()
 
   config_file_path = sys.argv[1]
   upload_file_path = sys.argv[2]
 
   if os.path.isfile(config_file_path) is False:
-    print "not found config file: "+config_file_path
+    print("not found config file: "+config_file_path)
     sys.exit()
 
   if os.path.isfile(upload_file_path) is False:
-    print "not found upload file: " + upload_file_path
+    print("not found upload file: " + upload_file_path)
     sys.exit()
 
   mimetype_upload_file = mimetypes.guess_type(upload_file_path)
 
-  #print type(mimetype_upload_file)
+  #print(type(mimetype_upload_file))
 
   upload_file_mimetype = mimetype_upload_file[0]
 
   if upload_file_mimetype is None:
-    print "could not get mimetype for upload file - using the default"
+    print("could not get mimetype for upload file - using the default")
     upload_file_mimetype = 'application/octet-stream'
 
   upload_file_title =  os.path.basename(upload_file_path)
@@ -64,20 +64,20 @@ try:
     config = json.loads(config_file.read())
   
   drive_service = helper.createDriveService(config)
-  print helper.coloured_output("Authentication is sucessful" , 'green')
-  print helper.print_about( drive_service )
-  print helper.coloured_output("Uploading file to your google drive" , 'yellow')
+  print(helper.coloured_output("Authentication is sucessful" , 'green'))
+  print(helper.print_about( drive_service ))
+  print(helper.coloured_output("Uploading file to your google drive" , 'yellow'))
 
   file_result = helper.insert_file( drive_service, config, upload_file_path, upload_file_title,upload_file_mimetype  )
   
-  print helper.coloured_output("Uploaded new file done" , 'green')
-  #print file_result
+  print(helper.coloured_output("Uploaded new file done" , 'green'))
+  #print(file_result
 
-  print helper.coloured_output("Getting list of children files" , 'yellow')
+  print(helper.coloured_output("Getting list of children files" , 'yellow'))
   children_files = helper.retrieve_all_files( drive_service, config['backup_folder_id'] )
-  # print "children_files", children_files
-  #print children_files
-  print "This folder have {0} files".format( str(len(children_files)) )
+  # print("children_files", children_files)
+  #print(children_files)
+  print("This folder have {0} files".format( str(len(children_files)) ))
 
   if len( children_files ) > config['max_file_in_folder']:
     #Remove old backup file
@@ -87,15 +87,15 @@ try:
 
     while count < number_delete_file:
       children_id = children_files[index_delete_file]['id']
-      print helper.coloured_output( "Removing old file with id " + children_id , 'yellow')
+      print(helper.coloured_output( "Removing old file with id " + children_id , 'yellow'))
       helper.remove_file( drive_service, children_id )
       count +=1
       index_delete_file -=1
 
-  print helper.coloured_output("Done backup file to google drive !" , 'green')
-except Exception, e:
-  print "error"
-  print e
+  print(helper.coloured_output("Done backup file to google drive !" , 'green'))
+except Exception as e:
+  print("error")
+  print(e)
 finally:
   pass
 
